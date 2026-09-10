019e171628bf7abfc7de18f847b8833e86e7d1a0
Arpita-sukalkar <arpitasukalkar@gmail.com>
2026-09-10 15:12:31 +0530

    changes

--- backend/src/main/java/com/yourorg/appname/Application.java
+++ backend/src/main/java/com/yourorg/appname/Application.java
@@ -3,7 +3,7 @@ package com.yourorg.appname;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplicationg
+@SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
